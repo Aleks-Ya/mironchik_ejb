1f48878cb6199a74e8dc478a0abce8e783ae2cb3
Aleksey Yablokov <ya_al@bk.ru>
2014-10-14 11:39:27 +0400

    Поиск в JNDI

--- day2/app1/View/src/ru/intf/view/Servlet.java
+++ day2/app1/View/src/ru/intf/view/Servlet.java
@@ -1,5 +1,10 @@
 package ru.intf.view;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,6 +33,29 @@ public class Servlet extends HttpServlet {
         out.print("<hr/>Привет, Мир!");
         Thread t = Thread.currentThread();
         out.print("<hr/>Thread =" + t);
+
+        Context ctx = null;
+        try {
+            ctx = new InitialContext();
+            NamingEnumeration<NameClassPair> ne = ctx.list("java:jboss");
+            while (ne.hasMoreElements()) {
+                Object o = ne.nextElement();
+                out.print("<hr/>" + o);
+            }
+            Object exampleDs = ctx.lookup("java:jboss/datasources/ExampleDS");
+            out.print("<hr/>" + exampleDs);
+        } catch (Exception e) {
+            e.printStackTrace(out);
+        } finally {
+            if (ctx != null) {
+                try {
+                    ctx.close();
+                } catch (NamingException e) {
+                    e.printStackTrace(out);
+                }
+            }
+        }
+
         out.print("</html>");
         out.close();
     }
