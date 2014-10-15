2f65b130e5c0856b381518fdcba80e299ea3439e
Aleksey Yablokov <ya_al@bk.ru>
2014-10-15 17:12:40 +0400

    EJB приложение

--- day3/AppWeb/ViewTest/src/ru/intf/viewtest/S1.java
+++ day3/AppWeb/ViewTest/src/ru/intf/viewtest/S1.java
@@ -1,5 +1,7 @@
 package ru.intf.viewtest;
 
+import ru.intf.model.FacadeRemote;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -13,6 +15,8 @@ import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/s1")
 public class S1 extends HttpServlet {
+    private FacadeRemote f;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
