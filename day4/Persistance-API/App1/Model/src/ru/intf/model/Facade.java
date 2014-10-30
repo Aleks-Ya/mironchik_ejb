98143758f680244a2fdfbc0a16014b6e9f6b5063
Aleksey Yablokov <ya_al@bk.ru>
2014-10-30 13:17:21 +0300

    Transaction Management

--- day4/Persistance-API/App1/Model/src/ru/intf/model/Facade.java
+++ day4/Persistance-API/App1/Model/src/ru/intf/model/Facade.java
@@ -1,18 +1,51 @@
 package ru.intf.model;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 @Stateless
+//@TransactionManagement(TransactionManagementType.CONTAINER)//По-умолчанию
+@TransactionManagement(TransactionManagementType.BEAN)
 public class Facade implements FacadeLocal {
     @PersistenceContext(unitName = "Model")
     private EntityManager em;
 
+    @Resource
+    private UserTransaction tx;
+
     @Override
     public String info() {
-        String t = em.toString();
+        TxInfo.txPrintInfo(tx);
+        String t = null;
+        try {
+            tx.begin();
+            t = em.toString();
+            TxInfo.txPrintInfo(tx);
 
+            int i = 0;
+            System.out.println(10 / i);
+
+            tx.commit();
+            TxInfo.txPrintInfo(tx);
+        } catch (NotSupportedException | SystemException | HeuristicRollbackException | RollbackException | HeuristicMixedException | ArithmeticException e) {
+            System.out.println("Error " + e);
+            try {
+                tx.rollback();
+                TxInfo.txPrintInfo(tx);
+            } catch (SystemException e1) {
+                e1.printStackTrace();
+            }
+        }
         return t;
     }
 }
